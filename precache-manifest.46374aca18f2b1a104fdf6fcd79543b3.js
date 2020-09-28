e29880b68629afc594dc51c5dcdfbf034e80f199
Kuntal Banerjee <kuntalbanerjee.de@gmail.com>
2020-09-29 03:25:01 +0530

    initial dist

---
+++ precache-manifest.46374aca18f2b1a104fdf6fcd79543b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bc4df76777be2f5e2007e1f468515d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e34ae5c678a91e6894",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cc2868c50ccb0f9662bd",
+    "url": "/static/js/2.d52dea9b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.d52dea9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e34ae5c678a91e6894",
+    "url": "/static/js/main.863cf3af.chunk.js"
+  },
+  {
+    "revision": "1c3249e6a41fe914c890",
+    "url": "/static/js/runtime-main.60987023.js"
+  }
+]);
